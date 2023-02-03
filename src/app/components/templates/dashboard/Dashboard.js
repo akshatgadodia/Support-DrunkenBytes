66d71317a88e88fb1692161edf86b4d39a66f4bd
akshatgadodia <akshatgadodia@gmail.com>
2023-02-03 21:44:43 +0530

    Initial Commit

---
+++ src/app/components/templates/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return(
+    <>
+    <div style={{height:'100vh', border:"10px solid black"}}>HomePage</div>
+    </>
+  );
+};
+
+export default Dashboard;
