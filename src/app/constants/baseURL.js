81510d0124d9a514c4b0954a2bcd28f1205e8c89
akshatgadodia <akshatgadodia@gmail.com>
2023-02-04 20:13:07 +0530

    Changes Pushed

--- src/app/constants/baseURL.js
+++ src/app/constants/baseURL.js
@@ -1,5 +1,5 @@
 let baseUrl;
-baseUrl = "http://localhost:5000/api";
-baseUrl = "https://api-drunkenbytes.onrender.com/api"
+baseUrl = "http://localhost:5000";
+baseUrl = "https://api-drunkenbytes.onrender.com"
 // baseUrl = "https://api-drunkenbytes.vercel.app/api"
 export default baseUrl;
