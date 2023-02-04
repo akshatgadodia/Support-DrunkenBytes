e42e8e813e919027666b105b79484f10c2031767
akshatgadodia <akshatgadodia@gmail.com>
2023-02-04 19:33:28 +0530

    Testing Something

--- src/app/constants/baseURL.js
+++ src/app/constants/baseURL.js
@@ -1,5 +1,5 @@
 let baseUrl;
 baseUrl = "http://localhost:5000/api";
-// baseUrl = "https://api-drunkenbytes.onrender.com/api"
+baseUrl = "https://api-drunkenbytes.onrender.com/api"
 // baseUrl = "https://api-drunkenbytes.vercel.app/api"
 export default baseUrl;
