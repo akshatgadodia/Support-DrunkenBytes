e1b5d130c64707bad7d5871bc579de8eb3068d20
akshatgadodia <akshatgadodia@gmail.com>
2023-02-04 16:35:01 +0530

    Testing Something

--- src/app/components/templates/dashboard/Dashboard.js
+++ src/app/components/templates/dashboard/Dashboard.js
@@ -1,9 +1,26 @@
+import baseURL from "@/app/constants/baseURL";
 import React from "react";
 
 const Dashboard = () => {
+  const login = async() => {
+    console.log(`${baseURL}/support-user/login`)
+    const data = await fetch(`${baseURL}/support-user/login`,{
+      method:'post',
+      headers: {'Content-Type':'application/json'},
+      body:JSON.stringify({
+        email: "akshatgadodia@gmail.com",
+        password: "qwerty"
+      }),
+      credentials: 'include',
+      mode:'cors'
+    });
+    console.log(data);
+    console.log(await data.json());
+  }
   return(
     <>
     <div style={{height:'100vh', border:"10px solid black"}}>HomePage</div>
+    <button onClick={login}>CLICK ME</button>
     </>
   );
 };
---
+++ src/app/constants/baseURL.js
@@ -0,0 +1,4 @@
+// const baseUrl = "http://localhost:5000/api";
+const baseUrl = "https://api-drunkenbytes.vercel.app/api"
+// export default baseUrl = "";
+export default baseUrl;
