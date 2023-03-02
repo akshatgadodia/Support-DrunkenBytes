f33038dae6dfeb1c109ee18b1ea83303f7e756d8
Akshita44 <akshitasharma407@gmail.com>
2023-03-02 16:05:23 +0530

    image

--- src/app/components/templates/articlesWritePage/ArticlesWritePage.js
+++ src/app/components/templates/articlesWritePage/ArticlesWritePage.js
@@ -4,33 +4,39 @@ import Head from "next/head";
 import { Button, Form, Input } from "antd";
 import dynamic from "next/dynamic";
 import { useHttpClient } from "@/app/hooks/useHttpClient";
-
+import {imageupload} from "@/app/components/elements/imageupload"
 let Editor = dynamic(() => import("@/app/components/modules/Editor"), {
     ssr: false
   });
 const ArticlesWritePage = () => {
   const { error, sendRequest, isLoading } = useHttpClient();
   const [articleData,setArticleData] = useState({});
+  const [mainphoto,setmainphoto]=useState("")
   const editorCore = React.useRef(null);
   const [form] = Form.useForm();
   const onFinish = async values => {
     try {
+      console.log(mainphoto);
       const savedData = await editorCore.current.save()
-      console.log(values);
-      console.log("Title");
-      console.log(values.title)
-      console.log("URL");
-      console.log(values.url)
-      console.log("BLOG DATA");
-      console.log(savedData);
+      // console.log(values);
+      // console.log("Title");
+      // console.log(values.title)
+      // console.log("URL");
+      // console.log(values.url)
+      // console.log("BLOG DATA");
+      // console.log(savedData);
+      // console.log(!savedData.blocks[0].data.text)
+      const data={title:values.title,url:values.url,content:JSON.stringify(savedData)}
+      if(mainphoto)
+      {
+        const res= await imageupload(mainphoto)
+        data.photo=res.file.url
+      }
+      console.log(data);
       const result = await sendRequest(
         `/article/save-article`,
         "POST",
-        JSON.stringify({
-          title: values.title,
-          url: values.url,
-          content:JSON.stringify(savedData),
-        })
+        JSON.stringify(data)
       ); 
       console.log(result);
       if (!error) {
@@ -79,6 +85,16 @@ const ArticlesWritePage = () => {
             className={styles.formItem}
           >
             <Input placeholder="Article URL" className={styles.input} />
+          </Form.Item>
+          <Form.Item 
+            rules={[
+              {
+                required: true,
+                message: "Please enter Main Photo"
+              }
+            ]}
+            className={styles.formItem}>
+          <Input placeholder="Add Main Photo" type="file" onChange={(e)=>{setmainphoto(e.target.files[0])}}></Input>
           </Form.Item>
             <Editor data={articleData} setData={setArticleData} editorCore={editorCore}/>
           <Form.Item>
--- src/app/components/templates/blogsWritePage/BlogsWritePage.js
+++ src/app/components/templates/blogsWritePage/BlogsWritePage.js
@@ -3,30 +3,38 @@ import styles from "./blogsWritePage.module.css";
 import Head from 'next/head';
 import { Button, Form, Input, DatePicker, Modal, Spin } from "antd";
 import dynamic from "next/dynamic";
+import {imageupload} from "@/app/components/elements/imageupload"
+import { useHttpClient } from "@/app/hooks/useHttpClient";
+
 let Editor = dynamic(() => import("@/app/components/modules/Editor"), {
   ssr: false
 });
 const BlogsWritePage = () => {
+  const { error, sendRequest, isLoading } = useHttpClient();
   const [blogData,setBlogData] = useState({});
+  const [mainphoto,setmainphoto]=useState("")
   const editorCore = React.useRef(null);
   const [form] = Form.useForm();
   const onFinish = async values => {
     try {
       const savedData = await editorCore.current.save()
-      console.log("Title");
-      console.log(values.title)
-      console.log("URL");
-      console.log(values.url)
-      console.log("BLOG DATA");
+      const data={title:values.title,url:values.url,content:JSON.stringify(savedData)}
+      if(mainphoto)
+      {
+        const res= await imageupload(mainphoto)
+        data.photo=res.file.url
+      }
+      // console.log(data);
+      // console.log("Title");
+      // console.log(values.title)
+      // console.log("URL");
+      // console.log(values.url)
+      // console.log("BLOG DATA");
       console.log(savedData);
       const result = await sendRequest(
         `/blog/save-blog`,
         "POST",
-        JSON.stringify({
-          title: values.title,
-          url: values.url,
-          content:JSON.stringify(savedData),
-        })
+        JSON.stringify(data)
       ); 
       console.log(result);
       if (!error) {
@@ -75,6 +83,16 @@ const BlogsWritePage = () => {
             className={styles.formItem}
           >
             <Input placeholder="Blog URL" className={styles.input} />
+          </Form.Item>
+          <Form.Item 
+            rules={[
+              {
+                required: true,
+                message: "Please enter Main Photo"
+              }
+            ]}
+            className={styles.formItem}>
+          <Input placeholder="Add Main Photo" type="file" onChange={(e)=>{setmainphoto(e.target.files[0])}}></Input>
           </Form.Item>
             <Editor data={blogData} setData={setBlogData} editorCore={editorCore}/>
           <Form.Item>
